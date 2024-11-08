e77129e167f9e0b2c5ed62ceae0b40eed4ac1068
LughenWolf <wolfblooder83390@gmail.com>
2024-11-08 09:32:24 +0100

    Update back mdp.php

--- back mdp.php
+++ back mdp.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire d'inscription</title>
+</head>
+<body>
+
 <?php
 // Connexion à la base de données
 $server = "localhost";
@@ -6,31 +12,46 @@ $username = "root";
 $password = "";
 $databaseName = "projet zoo";
 
-// Connexion à la base de données
 $conn = new mysqli($server, $username, $password, $databaseName);
 
 // Vérification de la connexion
 if ($conn->connect_error) {
-    die("Connexion à la BDD échouée");
+    die("Connexion à la BDD échouée : " . $conn->connect_error);
 }
-?>
-<HTML>
-<head>
+
+// Traitement du formulaire lors de la soumission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-TEST
-    </head>
-    <body>
-<form class="form">
-<input type="nom" id="nom" placeholder="entrez votre nom" />
-<br>
-<input type="prenom" id="prenom" placeholder="entrez votre prenom" />
-<br>
-<input type="email" id="email" placeholder="Entrez votre email"/>
-<br>
-<input type="password" id="password" placeholder="Entrez votre mot de
- passe"/>
-<br>
-<button>s inscrire</button>
+    $nom = $conn->real_escape_string($_POST['nom']);
+    $prenom = $conn->real_escape_string($_POST['prenom']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $password = $conn->real_escape_string($_POST['password']); // 
+
+    // Requête d'insertion
+    $sql = "INSERT INTO user nom,prenom,email,mdp) VALUES ('$nom', '$prenom', '$email', '$password')";
+
+    // Exécuter la requête et vérifier le succès
+    if ($conn->query($sql) === TRUE) {
+        echo "Données ajoutées avec succès";
+    } else {
+        echo "Erreur lors de l'insertion : " . $conn->error;
+    }
+}
+
+?>
+
+<!-- Formulaire HTML -->
+<form class="form" method="post" action="">
+    <input type="text" name="nom" id="nom" placeholder="Entrez votre nom" required />
+    <br>
+    <input type="text" name="prenom" id="prenom" placeholder="Entrez votre prénom" required />
+    <br>
+    <input type="email" name="email" id="email" placeholder="Entrez votre email" required />
+    <br>
+    <input type="password" name="password" id="password" placeholder="Entrez votre mot de passe" required />
+    <br>
+    <button type="submit">SIGN IN</button>
 </form>
+
 </body>
 </html>
